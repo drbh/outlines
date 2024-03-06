56fa0e2faa44170e4090e2ee878a36793a1c9678
drbh <david.richard.holtz@gmail.com>
2024-03-06 20:53:34 +0000

    feat: use library in tests

--- outlines/fsm/regex.py
+++ outlines/fsm/regex.py
@@ -491,7 +491,64 @@ def state_scan_tokens(
     return res
 
 
+import rustlines
+
+
+def create_fsm_index_end_to_end_using_rustlines_state_scan_tokens(
+    fsm_info: FSMInfo,
+    vocabulary: Dict[str, List[int]],
+) -> Dict[int, Set[Tuple[int, int]]]:
+    """Create an FSM state-to-vocabulary map/index through end-to-end token parsing."""
+
+    # TODO: Consider using a `List` of `Set`s instead; that way we can JIT this
+    # code, too.
+    states_to_token_subsets: Dict[int, Set[Tuple[int, int]]] = {}
+    seen: Set[int] = set()
+    next_states = {fsm_info.initial}
+
+    while next_states:
+        start_state = next_states.pop()
+
+        token_ids_end_states = rustlines.state_scan_tokens(
+            dict(fsm_info.transitions),
+            dict(fsm_info.alphabet_symbol_mapping),
+            fsm_info.alphabet_anything_value,
+            fsm_info.initial,
+            set(fsm_info.finals),
+            dict(vocabulary),
+            start_state,
+        )
+
+        for token_id_and_end_state in token_ids_end_states:
+            states_to_token_subsets.setdefault(start_state, set()).add(
+                token_id_and_end_state
+            )
+            end_state = token_id_and_end_state[1]
+            if end_state not in seen:
+                next_states.add(end_state)
+
+        seen.add(start_state)
+
+    return states_to_token_subsets
+
+
 def create_fsm_index_end_to_end(
+    fsm_info: FSMInfo,
+    vocabulary: Dict[str, List[int]],
+) -> Dict[int, Set[Tuple[int, int]]]:
+    """Create an FSM state-to-vocabulary map/index through end-to-end token parsing."""
+    res = rustlines.create_fsm_index_end_to_end_rust(
+        dict(fsm_info.transitions),
+        dict(fsm_info.alphabet_symbol_mapping),
+        fsm_info.alphabet_anything_value,
+        fsm_info.initial,
+        set(fsm_info.finals),
+        dict(vocabulary),
+    )
+    return res
+
+# The original implementation of `create_fsm_index_end_to_end` is below (renamed)
+def original_create_fsm_index_end_to_end(
     fsm_info: FSMInfo,
     vocabulary: Dict[str, List[int]],
 ) -> Dict[int, Set[Tuple[int, int]]]:
